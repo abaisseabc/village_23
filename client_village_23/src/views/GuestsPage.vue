9fcf1fc6453111115a9513a61d27fe0b839c17f2
abaisseabc <abaisseabc666@gmail.com>
2024-04-03 12:47:34 +0300

    fix(client_village_23): добавлена отправка на сервер форма для гостей

--- client_village_23/src/views/GuestsPage.vue
+++ client_village_23/src/views/GuestsPage.vue
@@ -8,22 +8,44 @@
 
       <div class="guests-page__service_first_lvl">
         <div>
-          <label>Введите номер комнаты</label> <br>
-          <input>
-        </div>
-        <div>
           <label>Введите ваш ID</label> <br>
-          <input>
+          <input v-model="service['guest_id_service']" style="width: 1030px;">
         </div>
       </div>
 
       <div class="guests-page__service_second_lvl">
         <div>
-          <label>Выберите усулгу</label> <br>
-          <input class="guests-page__service_second_lvl__input">
+          <label>Выберите услугу</label> <br>
+          <select class="guests-page__service_second_lvl__input" v-model="service['service_name']">
+            <option
+                class="guests-page__service_second_lvl__input"
+                value="Заменить полотенца"
+            >Заменить полотенца
+            </option>
+            <option
+                class="guests-page__service_second_lvl__input"
+                value="Убраться"
+            >Убраться
+            </option>
+            <option
+                class="guests-page__service_second_lvl__input"
+                value="Принести воды"
+            >Принести воды
+            </option>
+            <option
+                class="guests-page__service_second_lvl__input"
+                value="Починить оборудование"
+            >Принести воды
+            </option>
+            <option
+                class="guests-page__service_second_lvl__input"
+                value="Починить оборудование"
+            >Заказать такси
+            </option>
+          </select>
         </div>
         <div>
-          <button>Заказать</button>
+          <button @click="orderRoomService">Заказать</button>
         </div>
       </div>
     </div>
@@ -32,6 +54,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 import gpNavBar from "@/components/guests-page/gpNavBar.vue";
 
 export default {
@@ -39,6 +63,26 @@ export default {
   components: {
     gpNavBar,
   },
+  data() {
+    return {
+      service: {
+        "guest_id_service": "",
+        "service_name": "",
+      }
+    }
+  },
+  methods: {
+    async orderRoomService() {
+      await axios
+          .post("http://127.0.0.1:8000/api/v1/services/", this.service)
+          .then(response => {
+            if (response.status === 201) {
+              console.log(response);
+            }
+          })
+          .catch(err => console.log(err))
+    },
+  }
 }
 </script>
 
